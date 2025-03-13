414802020b559b929af9abf197c2565f7725b0b3
preethramadoss <167985196+preethramadoss@users.noreply.github.com>
2025-03-14 10:50:42 +1100

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route to serve index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"), (err) => {
         if (err) {
@@ -17,7 +15,6 @@ app.get("/", (req, res) => {
     });
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`✅ Server running at http://localhost:${port}`);
 });
